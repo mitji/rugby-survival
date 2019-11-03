f2d1dece303910cff56dede6e703f4ef00ad9f61
David Mitjana Castro <d.mitjana@gmail.com>
2019-11-03 14:43:46 +0100

    fix canvas dimensions

--- src/defender.js
+++ src/defender.js
@@ -19,7 +19,7 @@ Defender.prototype.handleDefenseMovement = function() {
   this.y = this.y + this.directionY * this.speed; // updates height of the player in every same
   this.isInBorderTop = false;
   this.isInBorderBottom = false;
-  if (this.y>= this.canvas.height) {
+  if (this.y>= (this.canvas.height-this.size)) {
     this.directionY = -1; 
     this.isInBorderBottom = true;
   }
