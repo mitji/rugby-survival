e3ae18578e31be6bb6dbe7cef5a20b210ac3e4f0
David Mitjana Castro <d.mitjana@gmail.com>
2019-11-06 15:32:43 +0100

    add ball icon

--- src/ball.js
+++ src/ball.js
@@ -3,7 +3,7 @@
 function Ball(canvas) {
   this.canvas = canvas;
   this.ctx = this.canvas.getContext('2d');
-  this.size = 10;
+  this.size = 30;
   this.x = canvas.width / 4;
   this.y = canvas.height / 2;
   this.directionX = 0; // initialise direction to the right
@@ -12,6 +12,7 @@ function Ball(canvas) {
 }
 
 Ball.prototype.draw = function() {
-  this.ctx.fillStyle = '#FF6F27';
-  this.ctx.fillRect(this.x, this.y, this.size, this.size);
+  var ballIcon = new Image();
+  ballIcon.src = './images/ball.png';
+  this.ctx.drawImage(ballIcon,this.x, this.y, this.size, this.size);
 }
